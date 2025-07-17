799b186eb89098f63a1fdd197035481a133d8c78
kmrnishantkmr <31732973+kmrnishantkmr@users.noreply.github.com>
2025-07-17 18:59:41 +0530

    Update updatefrontendnew.sh

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+
+get_instance_id() {
+  TOKEN=$(curl -sX PUT "http://169.254.169.254/latest/api/token" \
+    -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+  curl -sH "X-aws-ec2-metadata-token: $TOKEN" \
+    http://169.254.169.254/latest/meta-data/instance-id
+}
+
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
+# INSTANCE_ID="i-030da7d31a1dbbffc"
+INSTANCE_ID=$(get_instance_id)
+
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
